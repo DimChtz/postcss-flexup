bd5589349affa65baf522a84388dd0da1d44b0f5
Dimitris Chatzis <dim.chatzis1993@gmail.com>
2024-03-15 20:48:21 +0200

    refactor: Update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,10 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import terser from '@rollup/plugin-terser';
 import { babel } from '@rollup/plugin-babel';
+import * as pckg from './package.json';
+
+const year = new Date().getFullYear();
+const banner = `//! PostCSS Flexup v${pckg.version} Copyright (c) ${year} ${pckg.author}`;
 
 export default () => [{
   input: 'src/index.js',
@@ -10,13 +14,15 @@ export default () => [{
       file: 'dist/index.cjs',
       format: 'cjs',
       sourcemap: true,
-      exports: 'default'
+      exports: 'default',
+      banner: banner
     },
     {
       file: 'dist/index.mjs',
       format: 'esm',
       sourcemap: true,
-      exports: 'default'
+      exports: 'default',
+      banner: banner
     }
   ],
   plugins: [
