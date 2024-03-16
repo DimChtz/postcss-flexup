d2d7e439653ec85b6b91ea90086215a4ae21988b
Dimitris Chatzis <dim.chatzis1993@gmail.com>
2024-03-16 21:20:19 +0200

    Move __tests__ under src

--- src/__tests__/basic.js
+++ src/__tests__/basic.js
@@ -1,11 +1,11 @@
 import postcss from 'postcss';
-import plugin from '../src/index.js';
+import plugin from '../index.js';
 import path from 'path';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
 
 async function process(input, opts = {}) {
-  return await postcss([plugin(opts)]).process(input, {from: undefined});
+  return await postcss([plugin(opts)]).process(input, { from: undefined });
 }
 
 async function run(input, output, opts = {}) {
@@ -21,7 +21,9 @@ function loadFixtures(name) {
 
   return [
     fs.readFileSync(path.join(__dirname, `fixtures/${name}.css`)).toString(),
-    fs.readFileSync(path.join(__dirname, `fixtures/${name}.expected.css`)).toString()
+    fs
+      .readFileSync(path.join(__dirname, `fixtures/${name}.expected.css`))
+      .toString()
   ];
 }
 
